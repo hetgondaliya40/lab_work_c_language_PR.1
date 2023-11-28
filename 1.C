8e26f09f400bc30cc31e527c7dc6b2d16cfdd050
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2023-11-28 16:42:31 +0530

    Update 1.C

--- 1.C
+++ 1.C
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 void main(){
-	float c,f;
+	float celsius,fahrenheit;
 	clrscr();
 	printf("Enter the value of Celsius: ");
-	scanf("%f",&c);
-	f=(c*9/5)+32;
-	printf("\nThe value of Fahrenheit is: %f",f);
+	scanf("%f",&celsius);
+	fahrenheit=(celsius*9/5)+32;
+	printf("\nThe value of Fahrenheit is: %f",fahrenheit);
 	getch();
 }
